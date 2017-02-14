5c748888aa15ba97427c0d056e545a1894a10fbb
AndreasLF <andreas@lyhnefiehn.dk>
2017-02-14 17:31:48 +0100

    2.6

--- js/index1.js
+++ js/index1.js
@@ -1,10 +1,10 @@
 //Adressen på Bluetooth Mate, Password er 1234
 var macAddress = "00:06:66:7D:83:DF";
-var forwardButton, backwardButton, rightButton, leftButton, increaseButton, decreaseButton;
-var buttonHoldDown = false;
+var forwardButton, backwardButton, rightButton, leftButton, increaseButton, decreaseButton, speedCounterDiv;
+var speed = 110;
 
 
-function onLoad() {
+function onLoad() {    
     forwardButton = document.getElementById("forwardButton"); //Gets the foward button element
     backwardButton = document.getElementById("backwardButton"); //Gets the backward button element
     rightButton = document.getElementById("rightButton"); //Gets the right button element
@@ -12,6 +12,17 @@ function onLoad() {
 
     increaseButton = document.getElementById("increaseButton"); 
     decreaseButton = document.getElementById("decreaseButton");
+    speedCounterDiv = document.getElementById("speedCounterDiv");
+    
+    var controllerWidth = document.getElementById("controllerDiv").offsetWidth; //This retrieves the width of the controllerDiv
+    console.log(controllerWidth); //prints controllerWidth to console. Useful for testing and debugging
+    
+    rightButton.style.marginLeft = (controllerWidth - 48)/2 + 48 + "px"; 
+    leftButton.style.marginLeft = (controllerWidth - 48)/2 -48 + "px";
+    
+    decreaseButton.style.marginLeft = (controllerWidth - 3*48)/2 + "px";
+
+    
     
     
 	document.addEventListener("deviceready", onDeviceReady, false); //eventlistener; calls the onDeviceReady() function, when device is ready
@@ -26,8 +37,10 @@ function onLoad() {
     leftButton.addEventListener("touchend", stopMove, false); //calls the stopMove() function when on touchend
     
     increaseButton.addEventListener("touchstart", increaseSpeed, false);
-    decreaseButton.addEventListener("touchstart", decreaseSpeed, false);
+    increaseButton.addEventListener("touchend", setButtonStyleDefault ,false);
     
+    decreaseButton.addEventListener("touchstart", decreaseSpeed, false);
+    decreaseButton.addEventListener("touchend", setButtonStyleDefault ,false);    
     
 }
 
@@ -83,30 +96,80 @@ function onDisconnect() {
 //        alert("subscribe failed");
 //}
 	
-function moveForward() {   
-    sendToArduino("f"); 
+function moveForward() { 
+    forwardButton.style.color = "rgba(255, 255, 255, 0.5)";
+    forwardButton.style.boxShadow = "0px 3px 17px -1px rgba(0, 0, 0, 0.3)";
+
+   // sendToArduino("f"); 
 }
     
 function moveBackward() { 
+    backwardButton.style.color = "rgba(255, 255, 255, 0.5)";
+    backwardButton.style.boxShadow = "0px 3px 17px -1px rgba(0, 0, 0, 0.3)";
+    
     sendToArduino("b");
 }
 
-function moveRight() {   
+function moveRight() {  
+    rightButton.style.color = "rgba(255, 255, 255, 0.5)";
+    rightButton.style.boxShadow = "0px 3px 17px -1px rgba(0, 0, 0, 0.3)";
+    
     sendToArduino("r");
 }
 
 function moveLeft() {   
+    leftButton.style.color = "rgba(255, 255, 255, 0.5)";
+    leftButton.style.boxShadow = "0px 3px 17px -1px rgba(0, 0, 0, 0.3)";
+    
     sendToArduino("l");
 }
 
 function stopMove() {
+    setButtonStyleDefault();
     sendToArduino("s");
 }
 
 function increaseSpeed() {
-    sendToArduino("i");
+    increaseButton.style.color = "rgba(255, 255, 255, 0.5)";
+    increaseButton.style.boxShadow = "0px 3px 17px -1px rgba(0, 0, 0, 0.3)";
+    if(speed < 250) {
+        speed = speed + 10;
+        speedCounterDiv.innerHTML = speed;
+            
+        sendToArduino("i");
+    }    
 }
 
 function decreaseSpeed() {
-    sendToArduino("d");
+    decreaseButton.style.color = "rgba(255, 255, 255, 0.5)";
+    decreaseButton.style.boxShadow = "0px 3px 17px -1px rgba(0, 0, 0, 0.3)";
+    
+    
+    if(speed > 100) {        
+        speed = speed - 10;
+        speedCounterDiv.innerHTML = speed;
+        
+        sendToArduino("d");
+    }
+    
+}
+
+function setButtonStyleDefault() {
+    forwardButton.style.color = "#FFFFFF";
+    forwardButton.style.boxShadow = "0px 3px 17px -1px rgba(0, 0, 0, 0.15)";
+    backwardButton.style.color = "#FFFFFF";
+    backwardButton.style.boxShadow = "0px 3px 17px -1px rgba(0, 0, 0, 0.15)";
+    rightButton.style.color = "#FFFFFF";
+    rightButton.style.boxShadow = "0px 3px 17px -1px rgba(0, 0, 0, 0.15)";
+    leftButton.style.color = "#FFFFFF";
+    leftButton.style.boxShadow = "0px 3px 17px -1px rgba(0, 0, 0, 0.15)";
+    
+    if(speed > 100 && speed < 250) {
+    increaseButton.style.color = "#FFFFFF";
+    increaseButton.style.boxShadow = "0px 3px 17px -1px rgba(0, 0, 0, 0.15)";
+    decreaseButton.style.color = "#FFFFFF";
+    decreaseButton.style.boxShadow = "0px 3px 17px -1px rgba(0, 0, 0, 0.15)";
+    }
+
+
 }
